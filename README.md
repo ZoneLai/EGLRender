d451e64a5306518d5d0835b84d86bb761350544b
laizhongan <laizhongan@insta360.com>
2018-06-27 10:47:34 +0800

    init .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EGLRender
+
+This is EGL render, not GLSurfaceView and GLRender
---
+++ app/src/main/java/com/phuket/tour/opengl/renderer/FileUtil.java
@@ -0,0 +1,33 @@
+package com.phuket.tour.opengl.renderer;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class FileUtil {
+
+    public static void copyFileFromAssets(Context context, String fileName, File destFile) {
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            FileOutputStream fos = new FileOutputStream(destFile);
+            byte[] buffer = new byte[1024];
+            int byteCount;
+            while ((byteCount = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, byteCount);
+            }
+            fos.flush();
+            is.close();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean fileExists(String path) {
+        if (TextUtils.isEmpty(path)) return false;
+        return new File(path).exists();
+    }
+}
